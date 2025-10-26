9a249a9aae9c284faa234ae9f72e5688cf699156
thirteenhh <13wrwr@gmail.com>
2025-10-26 20:47:18 +0400

    Initial commit

---
+++ game.js
@@ -0,0 +1,18 @@
+const cards = [
+  {name: "Обычная", rarity: 70},
+  {name: "Редкая", rarity: 25},
+  {name: "Эпическая", rarity: 4},
+  {name: "Легендарная", rarity: 1}
+];
+
+function getRandomCard() {
+  const rand = Math.random() * 100;
+  let sum = 0;
+  for (const card of cards) {
+    sum += card.rarity;
+    if (rand <= sum) return card;
+  }
+}
+const tg = window.Telegram.WebApp;
+tg.ready();  // говорит Telegram, что игра загрузилась
+console.log(tg.initDataUnsafe.user); // инфо о пользователе
