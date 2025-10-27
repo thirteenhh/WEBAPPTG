18454b3134173917651291a6eec1a604226b90d9
thirteenhh <13wrwr@gmail.com>
2025-10-27 19:48:46 +0400

    upd

--- game.js
+++ game.js
@@ -160,9 +160,3 @@ document.addEventListener("DOMContentLoaded", () => {
     renderProfile();
   }
 });
-
-const img = document.createElement("img");
-img.src = card.image;
-img.alt = card.rarity;
-img.classList.add("profileCard");
-profileCardsContainer.appendChild(img);
